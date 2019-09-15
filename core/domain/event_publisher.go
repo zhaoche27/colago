fbc31b963c18c0c8ee1c4bb52206619c19d6d0a8
Che Zhao <zhaoche27@126.com>
2019-09-15 15:46:02 +0800

    event2

--- core/domain/event_publisher.go
+++ core/domain/event_publisher.go
@@ -2,7 +2,7 @@ package domain
 
 import (
 	"context"
-	event2 "github.com/zhaoche27/colago/core/event"
+	"github.com/zhaoche27/colago/core/event"
 )
 
 var EventPublisher = &eventPublisher{}
@@ -10,6 +10,6 @@ var EventPublisher = &eventPublisher{}
 type eventPublisher struct {
 }
 
-func (ep *eventPublisher) Publish(ctx context.Context, event event2.Event) error {
-	return event2.Bus.FireAll(ctx, event)
+func (ep *eventPublisher) Publish(ctx context.Context, e event.Event) error {
+	return event.Bus.FireAll(ctx, e)
 }
